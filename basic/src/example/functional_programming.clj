6212e310f9bacbb996e0eecee698070c19aa9fb0
neal <icen.liu913@gmail.com>
2016-03-27 22:32:39 +0800

    How to think in fuctional programming. Severial ways to achieve Fibonaccis.

---
+++ basic/src/example/functional_programming.clj
@@ -0,0 +1,47 @@
+(ns src.example.functional-programming)
+;bad idea about Fibonaccis
+(defn stack-consuming-fibo [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+ (stack-consuming-fibo (- n 1))
+             (stack-consuming-fibo (- n 2)))))
+;This is easyly cause StackOverFlow Error
+
+
+;tail recursion
+(defn tail-fibo [n]
+  (letfn [(fib
+            [current next n]
+            (if (zero? n)
+              current
+              (fib next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
+;This is better, but also cause StackOVerFlow Error when n is too big
+
+
+;self-recursion
+(defn recur-fibo [n]
+  (letfn [(fib
+            [current next n]
+            (if (zero? n)
+              current
+              (recur next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
+;This is way better
+
+
+;lazy-seq Fibonaccis
+(defn lazy-seq-fibo
+  ([]
+   (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+   (let [n (+ a b)]
+     (lazy-seq
+       (cons n (lazy-seq-fibo b n))))))
+
+
+;build on map and iterator
+(defn fibo []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
+;(println (rem (nth (fibo) 1000000) 1000))
