e3e86de92f451cc780c3de3989e840850a50c73a
Neal <icen.liu913@gmail.com>
2016-03-28 11:54:07 +0800

    more about functional programming

--- basic/src/example/functional_programming.clj
+++ basic/src/example/functional_programming.clj
@@ -44,4 +44,45 @@
 ;build on map and iterator
 (defn fibo []
   (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
-;(println (rem (nth (fibo) 1000000) 1000))
+;(println (nth (fibo) 1000000))
+
+
+
+; Fuctional Programming
+;Coute toss coin result for heads pairs, :h is for Head :t is for Tail.
+;Write all you code by yourself.
+(defn count-heads-pairs [coll]
+  (loop [cnt 0 coll coll]
+    (if (empty? coll)
+      cnt
+      (recur (if (= :h (first coll) (second coll))
+               (inc cnt)
+               cnt)
+             (rest coll)))))
+
+
+;Use clojure seq-libraries
+(defn by-pair [coll]
+  (let [take-pairs (fn [c]
+                     (when (next c) (take 2 c)))]
+    (lazy-seq
+      (when-let [pair (seq (take-pairs coll))]
+        (cons pair (by-pair (rest coll)))))))
+(defn count-heads-pairs2 [coll]
+  (count (filter (fn [pair] (every? #(= :h %) pair))
+                 (by-pair [coll]))))
+
+;Clojure already hava some method can do by-pair thing called partition
+(def ^{:doc "Count items matching a filter"}
+   count-if (comp count filter))
+
+(defn count-runs
+  "Count runs of length n where pred is true in coll"
+  [n pred coll]
+  (count-if #(every? pred %) (partition n 1 coll)))
+
+(def ^{:doc "Count runs of length two that are both heads"}
+ count-head-pairs3 (partial count-runs 2 #(= % :h)))
+
+
+
